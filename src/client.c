28b9227e284191d3718844be25f597e58d5ea609
Noah Adjemian--Roseau <nadjemia@made-f0Br6s2.clusters.42paris.fr>
2024-01-13 13:40:32 +0100

    le serveur recoit bien les signaux en binaire

--- src/client.c
+++ src/client.c
@@ -6,31 +6,84 @@
 /*   By: nadjemia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:52:30 by nadjemia          #+#    #+#             */
-/*   Updated: 2024/01/13 11:03:12 by nadjemia         ###   ########.fr       */
+/*   Updated: 2024/01/13 12:59:07 by nadjemia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include "../include/libft.h"
-#include "../include/ft_printf.h"
-#include "../include/get_next_line.h"
+#include "../include/minitalk.h"
 
-int main(int argc, char **argv)
+char	*convert_bin(char *str);
+void	fill(char *bin, int c, int *i);
+void	send(char *bin, int pid);
+
+int	main(int argc, char **argv)
 {
-	int	pid;
-	int	i;
-	int	count;
+	int		pid;
+	char	*bin;
+	int		i;
 
+	i = 0;
 	if (argc != 3)
 		return (1);
 	pid = ft_atoi(argv[1]);
-	count = ft_atoi(argv[2]);
-	i = 0;
-	while (i++ < count)
-	{
-		kill(pid, SIGUSR1);
-		usleep(10000);
-	}
-	kill(pid, SIGUSR2);
+	bin = convert_bin(argv[2]);
+	if (!bin)
+		return (2);
+	send(bin, pid);
+	free(bin);
 	return (0);
+}
+
+void	send(char *bin, int pid)
+{
+	int		i;
+
+	i = 0;
+	while (bin[i])
+	{
+		if (bin[i] == '0')
+		{
+			kill(pid, SIGUSR1);
+			usleep(10000);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+			usleep(10000);
+		}
+		i++;
+	}
+}
+
+char	*convert_bin(char *str)
+{
+	char	*bin;
+	size_t	len;
+	int		i;
+	int		x;
+
+	x = 0;
+	i = 0;
+	len = ft_strlen(str) * 8;
+	bin = malloc(sizeof(char) * (len + 1));
+	if (!bin)
+		return (NULL);
+	while (str[i])
+	{
+		fill(bin, str[i], &x);
+		i++;
+	}
+	bin[x] = '\0';
+	return (bin);
+}
+
+void	fill(char *bin, int c, int *i)
+{
+	if (c >= 2)
+	{
+		fill(bin, c / 2, i);
+		fill(bin, c % 2, i);
+	}
+	else
+		bin[(*i)++] = '0' + c;
 }
--- src/server.c
+++ src/server.c
@@ -6,47 +6,66 @@
 /*   By: nadjemia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 11:03:30 by nadjemia          #+#    #+#             */
-/*   Updated: 2024/01/13 11:09:45 by nadjemia         ###   ########.fr       */
+/*   Updated: 2024/01/13 13:32:34 by nadjemia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include "../include/libft.h"
-#include "../include/ft_printf.h"
+#include "../include/minitalk.h"
 
-int	count = 0;
+char	buf[1000000] = {0};
 
+void	handler0(int sig);
 void	handler1(int sig);
-void	handler2(int sig);
+void	clean(char *buf);
 
 int	main(void)
 {
-	struct sigaction	recep;
-	struct sigaction	print;
+	struct sigaction	zero;
+	struct sigaction	un;
 
-	recep.sa_handler = handler1;
-	print.sa_handler = handler2;
-	sigaction(SIGUSR1, &recep, NULL);
-	sigaction(SIGUSR2, &print, NULL);
+	zero.sa_handler = handler0;
+	un.sa_handler = handler1;
+	sigaction(SIGUSR1, &zero, NULL);
+	sigaction(SIGUSR2, &un, NULL);
 	ft_printf("PID = %d\n", getpid());
 	while (1)
 		sleep(1);
 	return (0);
 }
 
-void	handler1(int sig)
+void	handler0(int sig)
 {
-	if (sig != SIGUSR1)
-		return ;
-	count++;
+	int		i;
+
+	i = 0;
+	if (sig == SIGUSR1)
+	{
+		ft_printf("0");
+		while (buf[i])
+			i++;
+		buf[i] = '0';
+	}
 }
 
-void	handler2(int sig)
+void	handler1(int sig)
 {
-	if (sig != SIGUSR2)
-		return ;
-	ft_printf("%d\n", count);
-	count = 0;
+	int		i;
+
+	i = 0;
+	if (sig == SIGUSR2)
+	{
+		ft_printf("1");
+		while (buf[i])
+			i++;
+		buf[i] = '1';
+	}
+}
+
+void	clean(char *buf)
+{
+	int		i;
+
+	i = 0;
+	while (buf[i])
+		buf[i++] = '\0';
 }
